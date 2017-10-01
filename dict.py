b9c469a9465585aaed6b18f23e3868d45ad3d90d
Hemanthtm2 <hemanthtm2@gmail.com>
2017-10-02 01:36:04 +0530

    first dictionary

---
+++ dict.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python 
+
+
+eng2sp={}
+
+eng2sp['one']='uno'
+eng2sp['two']='dos'
+
+print eng2sp
--- funbucket.py
+++ funbucket.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python 
 
 def inBucket(t,low,high):
-  t=[10,20,30,40]
+  t=[10,20,30,40,50,60,70,80,100]
   count=0
   for num in t:
     if low<num<high:
       count=count+1
   return count 
 
-print inBucket('t',20,40)
+#a=[10,20,30,40,50,60,70,80,100]
+print inBucket('t',20,100)
 
 
