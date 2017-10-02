679b1d26a977ff2a9e68b2d9f4232369059a5a74
Hemanthtm2 <hemanthtm2@gmail.com>
2017-10-02 16:25:47 +0530

    more reading file

---
+++ fileread.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python 
+
+f=open("name.txt","w")
+f.write("My name is Python!!!")
+f.close()
+#name=f.read()
+#print name
+
+f=open("name.txt","r")
+
+#print "\n", f.read()
+
+print f.read(7)
